3845eaa583299d20fde693e1afd6ca3fd7d76df8
Gunj Joshi <gunjjoshi8372@gmail.com>
2024-10-17 22:28:17 +0530

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -7,16 +7,15 @@ import { getFirestore, collection, getDoc } from 'firebase/firestore';
 import { doc, setDoc } from 'firebase/firestore';
 
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD7t2qU2EfuIihZqflWqdz2wteUAitYda8",
-    authDomain: "csyclub-1e1af.firebaseapp.com",
-    databaseURL: "https://csyclub-1e1af-default-rtdb.firebaseio.com",
-    projectId: "csyclub-1e1af",
-    storageBucket: "csyclub-1e1af.appspot.com",
-    messagingSenderId: "310772231026",
-    appId: "1:310772231026:web:93964a0338e64a4dd88beb",
-    measurementId: "G-M0SG58E4TG"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 
