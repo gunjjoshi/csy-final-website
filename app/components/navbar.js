cfd00b3d1be9a9efce32df922dd1db2cac1cf53c
Gunj Joshi <gunjjoshi8372@gmail.com>
2024-11-01 17:10:18 +0530

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -106,14 +106,11 @@ const Navbar = () => {
         }
     };
 
-
-
     return (
         <AuthContextProvider>
             <div>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
                 <link href="https://fonts.cdnfonts.com/css/cascadia-code" rel="stylesheet" />
-
                 <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top" style={{ fontFamily: 'Cascadia Code, monospace' }}>
                     <div class="container">
                         <div className={styles.navLogo}>
